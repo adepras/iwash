5f48ce415e127720c168f59339d62fe82fad7e35
AdePrasTyo <adeprasestyo6@gmail.com>
2024-07-27 21:51:01 +0700

    Detail Order dan Bayar

--- resources/views/menu/detail_order.blade.php
+++ resources/views/menu/detail_order.blade.php
@@ -7,11 +7,74 @@
 
     <div class="container-detail-order py-5">
         <h2>Pesan Layanan iWash</h2>
+        <a href="{{ url()->previous() }}"><img src="image/back-to.svg" alt="">Kembali ke menu layanan</a>
         <div class="container-detail mt-5 mb-2">
             <div class="layout-order">
                 <h5>Detail Pemesanan dan Pembayaran</h5>
                 <div class="detail-order">
-
+                    <div class="pay-logo">
+                        <img src="image/iwash-full-logo.png" alt="">
+                        <h6>ID Pemesanan : 1eadew22337f7sefe7dw4</h6>
+                    </div>
+                    <hr class="line">
+                    <div class="detail-title">
+                        <h6>Paket Satu Kali Cuci</h6>
+                        <p>Basic</p>
+                        <p>Spot Remover - Body</p>
+                        <ul>
+                            <li>Total Harga :</li>
+                            <li>Rp.110.000</li>
+                        </ul>
+                        <ul>
+                            <li>Total Estimasi :</li>
+                            <li>1 jam 30 menit</li>
+                        </ul>
+                        <hr class="line">
+                        <h6>Data Diri</h6>
+                        <ul>
+                            <li>Nama :</li>
+                            <li>Ade Prasetyo</li>
+                        </ul>
+                        <ul>
+                            <li>Nomor WhatsApp:</li>
+                            <li>+62 812 2233 4455</li>
+                        </ul>
+                        <hr class="line">
+                        <h6>Data Kendaraan</h6>
+                        <ul>
+                            <li>Ukuran Kendaraan :</li>
+                            <li>-</li>
+                        </ul>
+                        <ul>
+                            <li>Brand :</li>
+                            <li>Totoya</li>
+                        </ul>
+                        <ul>
+                            <li>Type :</li>
+                            <li>GR Corolla</li>
+                        </ul>
+                        <ul>
+                            <li>Nomor Polisi :</li>
+                            <li>D 3 PRAS</li>
+                        </ul>
+                        <hr class="line">
+                        <h6>Jadwal Pemesanan</h6>
+                        <ul>
+                            <li>Tanggal :</li>
+                            <li>27/07/2024</li>
+                        </ul>
+                        <ul>
+                            <li>Waktu :</li>
+                            <li>12:00 WIB</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="order-menu pay-now mt-3">
+                    <div class="pay-price">
+                        <p>Total Pembayaran</p>
+                        <h6>Rp.110.000</h6>
+                    </div>
+                    <button class="btn-next" type="submit">Bayar</button>
                 </div>
             </div>
             <div class="layout-faq">
